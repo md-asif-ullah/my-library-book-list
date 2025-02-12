73685fafaa45b8f2f6261e7167d97e4e2c68746d
md-asif-ullah <mdasifullah456@gmail.com>
2025-02-12 17:02:29 +0600

    fixed: fixed schema import bugs

--- src/lib/ConnectToDB.tsx
+++ src/lib/ConnectToDB.tsx
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "@/db";
+import * as schema from "@/db/index";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
